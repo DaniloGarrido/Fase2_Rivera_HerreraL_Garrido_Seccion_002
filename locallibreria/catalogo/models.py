e83c16dac9ef9e6a29e83f61a77a9161cd8f3dc9
unknown <danilo.garrido156@gmail.com>
2020-10-28 23:09:51 -0300

    Creando pagina de inicio

--- locallibreria/catalogo/models.py
+++ locallibreria/catalogo/models.py
@@ -15,11 +15,11 @@ class Producto(models.Model):
     marca = models.ManyToManyField(Marca, help_text="Seleccione una marca para este producto")
     modelo=models.CharField(max_length=50,help_text='Modelo del producto')
     precio=models.PositiveIntegerField(blank=False,help_text='Precio del producto')
-    color=models.CharField(max_length=25, null=True, blank=True,help_text='Color del producto, solo si se puede')
-    peso=models.DecimalField(decimal_places=2, max_digits=4, null=True,blank=True,help_text='Peso del producto (solo si se espesifica)')
-    alto=models.DecimalField(decimal_places=2, max_digits=4, null=True,blank=True,help_text='Alto del produscto(Solo si espesifica)')
-    ancho=models.DecimalField(decimal_places=2, max_digits=4, null=True,blank=True,help_text='Ancho del producto(solo si se especifica)')
-    
+    color=models.CharField(max_length=25, null=True, blank=True,help_text='Color del producto(Puede quedar en blanco)')
+    peso=models.DecimalField(decimal_places=2, max_digits=4, null=True,blank=True,help_text='Peso del producto (puede quedar en blanco)')
+    alto=models.DecimalField(decimal_places=2, max_digits=4, null=True,blank=True,help_text='Alto del produscto(puede quedar en blanco)')
+    ancho=models.DecimalField(decimal_places=2, max_digits=4, null=True,blank=True,help_text='Ancho del producto(puede quedar en blanco)')
+    imagen=models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)    
     def __str__(self):
         return self.nombre
 
--- locallibreria/catalogo/views.py
+++ locallibreria/catalogo/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Producto, Marca
+from django.views import generic
 
 # Create your views here.
+def index(request):
+    num_productos=Producto.objects.all().count()
+    num_marcas=Marca.objects.all().count()
+
+    return render(
+        request,
+        'index.html',
+        context={'num_productos':num_productos,'num_marcas':num_marcas},
+    )
